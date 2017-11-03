5dfed786ff70ce04529bf49e01e5473f6334cd1d
Riku Inoue <riku.inoue@shiroyagi.co.jp>
2017-11-03 14:27:30 +0900

    Fix model field name

--- register/models.py
+++ register/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+__all__ = [
+    'Item',
+    'Order'
+]
+
+
 # User (Django標準) -< Item -< Order の関係
 
 class Item(models.Model):
     name = models.CharField('商品名', max_length=255)
     price = models.PositiveIntegerField('価格')
-    member_price = models.IntegerField('部員価格')
+    member_price = models.PositiveIntegerField('部員価格')
     quantity = models.PositiveIntegerField('数量')
 
-    user = models.ForeignKey(User, verbose_name='出品者', related_name='items', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, verbose_name='出品者', related_name='items', on_delete=models.PROTECT)
 
     def __str__(self):
-        return '商品'
+        return self.name
 
 
 class Order(models.Model):
@@ -20,7 +26,7 @@ class Order(models.Model):
     quantity = models.PositiveIntegerField('数量')
     created_at = models.DateTimeField('決済日', auto_now_add=True)
 
-    item = models.ForeignKey('Item', verbose_name='商品', related_name='orders', on_delete=models.CASCADE)
+    item = models.ForeignKey('Item', verbose_name='商品', related_name='orders', on_delete=models.PROTECT)
 
     def __str__(self):
-        return '会計'
+        return f'{self.item.name} x{self.quantity}'
