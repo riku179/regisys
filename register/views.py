66e4d14e9a56f91317f5b7d24db805ba8c197cab
Riku Inoue <riku.inoue@shiroyagi.co.jp>
2017-11-12 13:57:53 +0900

    Add aggregation Order api

--- register/views.py
+++ register/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets, mixins, response, status, permissions
+from rest_framework.decorators import list_route
+from rest_framework.request import Request
 from django.db.models.deletion import ProtectedError
+import re
 from .serializers import *
 from .models import *
 from .permissions import *
@@ -33,7 +36,7 @@ class ItemViewSet(viewsets.ModelViewSet):
         try:
             self.perform_destroy(instance)
         except ProtectedError:
-            return response.Response(status=status.HTTP_400_BAD_REQUEST)
+            return response.Response(status=status.HTTP_400_BAD_REQUEST, data='この商品で既に会計がされています')
         else:
             return response.Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -55,3 +58,36 @@ class OrderViewSet(mixins.CreateModelMixin,
             return ReadOrderSerializer
         else:
             return WriteOrderSerializer
+
+
+    @list_route()
+    def aggregate(self, request: Request):
+        """
+        出品者毎の期間中の総売上金
+        クエリパラメータfromとtoが必須('2017-01-01 01:01:01'をurlエンコードした形式)
+        """
+        dt_from = request.query_params.get('from')
+        dt_to = request.query_params.get('to')
+
+        if dt_from is None or dt_to is None:
+            return response.Response(status=status.HTTP_400_BAD_REQUEST, data='期間を指定してください')
+
+        if re.search(r'^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$', dt_from) is None \
+            or re.search(r'^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$', dt_to) is None:
+            return response.Response(status=status.HTTP_400_BAD_REQUEST, data='データの形式が不正です')
+
+        rows = Order.objects.raw('''
+        SELECT orders.id, auth_user.username, sum(orders.price * orders.quantity) AS 'sales' \
+        FROM orders \
+        JOIN items ON orders.item_id = items.id \
+        JOIN auth_user ON items.owner_id = auth_user.id \
+        WHERE created_at BETWEEN %s AND %s \
+        GROUP BY items.owner_id''', [dt_from, dt_to])
+
+        results = dict()
+        for row in rows:
+            results[row.username] = int(row.sales)
+        import sys
+        print(results, file=sys.stderr)
+
+        return response.Response(data=results)
